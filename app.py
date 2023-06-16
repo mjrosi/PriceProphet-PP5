970fd7bdcd7db0395eda39f745209b87f92d46eb
mjrosi <>
2023-06-16 11:39:22 +0000

    feat: Create Streamlit app and dashboard pages

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+from app_pages.page_summary import page_summary_body
+
+
+app = MultiPage(app_name= "PriceProphet: Housing Price Predictor") # Create an instance of the app 
+
+
+# load pages scripts
+app.add_page("Quick Project Summary", page_summary_body)
+
+
+
+app.run() # Run the  app
